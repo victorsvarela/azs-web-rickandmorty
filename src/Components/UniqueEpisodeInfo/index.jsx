766ee98b8c0f9668bc12146dbe25a351770ea10a
Victor Varela <vitaotapioqueiro@gmail.com>
2022-04-22 11:34:37 -0300

    fix: moved querys to services

--- src/Components/UniqueEpisodeInfo/index.jsx
+++ src/Components/UniqueEpisodeInfo/index.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router";
 import { useQuery } from "@apollo/client";
-import { EpisodeInfo } from "../../Routes/querys";
+import { EpisodeInfo } from "../../services/querys";
 import { useEffect, useState } from "react";
 
 import { Card, CharactersThisEpisode, CardCharacterUnique } from "./style";
